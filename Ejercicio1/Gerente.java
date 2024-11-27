d78764dae403c502f407bb1bcd42f18ef8426b2e
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-11-27 00:16:00 -0500

    Taller 4

---
+++ Ejercicio1/Gerente.java
@@ -0,0 +1,17 @@
+package Taller4.Ejercicio1;
+
+public class Gerente extends Empleado {
+    protected String departamento;
+
+    public Gerente(String nombre, double salario, String departamento) {
+         super(nombre, salario); 
+         this.departamento = departamento; 
+
+        } 
+        
+        @Override 
+        public void mostrarInformacion() { 
+            super.mostrarInformacion(); 
+            System.out.println("Departamento: " + departamento); 
+        }
+}
---
+++ Ejercicio2/Moto.java
@@ -0,0 +1,16 @@
+package Taller4.Ejercicio2;
+
+public class Moto extends Vehiculo {
+    private int cilindrada; 
+
+    public Moto(String tipo, String marca, int cilindrada) { 
+        super(tipo, marca);
+        this.cilindrada = cilindrada;
+     } 
+
+     @Override 
+     public void mostrarDetalles() {
+         super.mostrarDetalles(); 
+         System.out.println("Cilindrada: " + cilindrada + " cc");
+         }
+}
